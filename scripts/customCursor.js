0d74b3a5b701d319472298a574522b4b3e0fc241
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2023-05-27 00:07:19 +0200

    Weboldal Kinézeti Változtatások: Custom Cursor Elkészítése + Implementálása, Használata nem akadályozza user expereince-t (Hover effect-ek...). Content Elrendezése + Welcome Image Moving Border Effect (Neon) megalkotása. Welcome Image kicserélése -> Notepad related (Jobban Illik a kontextushoz). Background Grid Squares Kinézet + Szín megváltoztatása -> Környezetbe Illeszkedik és feldobja a hátteret. Kód rendezettés átlátható, ellenben weboldal nem teljesen responsive és egyes effectek kikapcsolása kisebb eszközökön még nem befejezett. Kommentek javítása és megírása egyes helyeken.

---
+++ scripts/customCursor.js
@@ -0,0 +1,9 @@
+// Get Cursor Current Position  and USe it
+function updateCursorPos(e) {
+  let circleCursor = document.querySelector('.circle-cursor');
+  circleCursor.style.left = e.clientX + 'px';
+  circleCursor.style.top = e.clientY + 'px';
+}
+
+document.addEventListener('mousemove', updateCursorPos);
+document.addEventListener('scroll', updateCursorPos);
