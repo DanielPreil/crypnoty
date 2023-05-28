0fbeb446c4564e0b064bae4aea91c33d55e1e3d7
Tóth Fazekas Balázs <tothfazekasbalazs@gmail.com>
2023-05-28 19:38:44 +0200

    A notesz oldal működő képesen be lett másolva a hozzá kellő fájlokkal. Problémek a főoldallal cél ennek javítása.

---
+++ scripts/crypto/monero.js
@@ -0,0 +1,17 @@
+let coinPriceTether = new WebSocket('wss://ws.coincap.io/prices?assets=monero'); 
+let stockPriceElementTether = document.getElementById('monero-price');
+let lastPriceTether = null;
+
+coinPriceTether.onmessage = (evt) => {
+    let stockObject = JSON.parse(evt.data);
+    let price = parseFloat(stockObject.monero).toFixed(2);
+    stockPriceElementTether.innerText = price + ' $';
+    stockPriceElementTether.style.color = lastPriceTether === null || lastPriceTether === price ? 'white': price > lastPriceTether ? 'lightgreen' : 'red';
+    lastPriceTether = price;
+};
+
+coinPriceTether.onerror = (evt) => {
+  console.error('WebSocket error:', evt);
+  stockPriceElementTether.innerText = 'Error fetching stock price.';
+  coinPriceTether.close();
+};
